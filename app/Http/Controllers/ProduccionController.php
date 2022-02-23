1c2979045f2ffccfba1408f719fa88c5734ff30d
Alex Suarez <alexotrowe@gmail.com>
2022-02-23 14:38:37 -0500

    Joel App v0.0.1

--- app/Http/Controllers/ProduccionController.php
+++ app/Http/Controllers/ProduccionController.php
@@ -38,12 +38,12 @@ class ProduccionController extends Controller
             DB::beginTransaction();
 
 
-            if(date('Y', strtotime($request->input('fecha_documento'))) != date('Y', strtotime(\Carbon\Carbon::now()))){
-                throw new \Exception('Solo se puede registrar documentos con el año actual', 1);
-            }
-            if(date('m', strtotime($request->input('fecha_documento'))) != date('m', strtotime(\Carbon\Carbon::now()))){
-                throw new \Exception('Solo se puede registrar documentos con el mes actual', 1);
-            }
+            // if(date('Y', strtotime($request->input('fecha_documento'))) != date('Y', strtotime(\Carbon\Carbon::now()))){
+            //     throw new \Exception('Solo se puede registrar documentos con el año actual', 1);
+            // }
+            // if(date('m', strtotime($request->input('fecha_documento'))) != date('m', strtotime(\Carbon\Carbon::now()))){
+            //     throw new \Exception('Solo se puede registrar documentos con el mes actual', 1);
+            // }
             
             $produccion = new Produccion();
             $totalDocumento = 0;
